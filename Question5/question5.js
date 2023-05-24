f15e8d504b0fc246b883d9265eab2b2f9ca5e5ef
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-24 05:21:00 -0700

    added

---
+++ Question5/question5.js
@@ -0,0 +1,15 @@
+function add(a, b, c) {
+  return a + b + c;
+}
+
+// Currying the add function
+function curryAdd(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+// Using the curried function
+console.log(curryAdd(2)(3)(4)); // Output: 9
