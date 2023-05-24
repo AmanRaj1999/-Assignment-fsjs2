4b52b0fc0d45ecbe1dd0a6443054e1d7c0223ca5
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-24 05:13:47 -0700

    added

---
+++ Question4.2/script.js
@@ -0,0 +1,31 @@
+outer.addEventListener(
+  "click",
+  function () {
+    console.log("Outer div clicked");
+  },
+  true
+);
+
+middle.addEventListener(
+  "click",
+  function () {
+    console.log("Middle div clicked");
+  },
+  true
+);
+
+inner.addEventListener(
+  "click",
+  function () {
+    console.log("Inner div clicked");
+  },
+  true
+);
+
+button.addEventListener(
+  "click",
+  function () {
+    console.log("Button clicked");
+  },
+  true
+);
